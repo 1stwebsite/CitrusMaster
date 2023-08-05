ff52fee67d929ab7f2f3c22e9ecd079f1f3418ae
1stweb.site <64778271+1stwebsite@users.noreply.github.com>
2023-08-05 15:24:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 // Функция для получения данных из Google Таблицы
 function fetchDataFromGoogleSheet() {
   const sheetID = '1G-3wbvdkQGlO64QLMe5gMd4M2epPzF4ohohhYBrb-oM'; // Замените на ID вашей Google Таблицы
-  const apiKey = 'AIzaSyAoZ92_SYkSos0qZY7XXL6njvUVGkwCgFk'; // Замените на ваш API ключ
-  const range = 'Sheet1!A1:G14'; // Диапазон ячеек, которые вы хотите получить
+  const apiKey = 'YOUR_API_KEY'; // Замените на ваш API ключ
+  const range = 'Sheet1!A1:G14'; // Диапазон ячеек, который вы хотите получить
 
   const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${range}?key=${apiKey}`;
 
   fetch(url)
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
     .then((data) => {
-      const values = data.values;
-      const table = document.getElementById('data-table');
+      if (data.values && data.values.length > 0) {
+        const values = data.values;
+        const table = document.getElementById('data-table');
 
-      for (const entry of values) {
-        const row = document.createElement('tr');
-        for (const value of entry) {
-          const cell = document.createElement('td');
-          cell.textContent = value;
-          row.appendChild(cell);
+        for (const entry of values) {
+          const row = document.createElement('tr');
+          for (const value of entry) {
+            const cell = document.createElement('td');
+            cell.textContent = value;
+            row.appendChild(cell);
+          }
+          table.appendChild(row);
         }
-        table.appendChild(row);
+      } else {
+        console.log('No data available.');
       }
     })
     .catch((error) => console.log('Error fetching data:', error));
