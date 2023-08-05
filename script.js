aebfeb21719f0a13674842c6c3034e66abc1be20
1stweb.site <64778271+1stwebsite@users.noreply.github.com>
2023-08-05 15:21:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Функция для получения данных из Google Таблицы
 function fetchDataFromGoogleSheet() {
-  const sheetID = '1G-3wbvdkQGlO64QLMe5gMd4M2epPzF4ohohhYBrb-oM'; // Replace with your Google Sheet ID
-  const apiKey = 'AIzaSyAoZ92_SYkSos0qZY7XXL6njvUVGkwCgFk'; // Replace with your API key
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/Sheet1?key=${apiKey}`;
+  const sheetID = '1G-3wbvdkQGlO64QLMe5gMd4M2epPzF4ohohhYBrb-oM'; // Замените на ID вашей Google Таблицы
+  const apiKey = 'AIzaSyAoZ92_SYkSos0qZY7XXL6njvUVGkwCgFk'; // Замените на ваш API ключ
+  const range = 'Sheet1!A1:G14'; // Диапазон ячеек, которые вы хотите получить
+
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${range}?key=${apiKey}`;
 
   fetch(url)
     .then((response) => response.json())
@@ -22,24 +24,3 @@ function fetchDataFromGoogleSheet() {
     })
     .catch((error) => console.log('Error fetching data:', error));
 }
-
-// Анимация появления таблицы
-function animateTable() {
-  const table = document.getElementById('data-table');
-  table.style.opacity = 0;
-
-  let opacity = 0;
-  const animationInterval = setInterval(() => {
-    opacity += 0.05;
-    table.style.opacity = opacity;
-    if (opacity >= 1) {
-      clearInterval(animationInterval);
-    }
-  }, 100);
-}
-
-// Получение данных и запуск анимации при загрузке страницы
-document.addEventListener('DOMContentLoaded', () => {
-  fetchDataFromGoogleSheet();
-  animateTable();
-});
