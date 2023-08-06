9661563c77f8bd1ea63376dc4aa4a946b9ba73d3
1stweb.site <64778271+1stwebsite@users.noreply.github.com>
2023-08-06 13:15:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Функция для получения данных из Google Таблицы
 function fetchDataFromGoogleSheet() {
-  const sheetID = '1G-3wbvdkQGlO64QLMe5gMd4M2epPzF4ohohhYBrb-oM#gid=1012221624'; // Замените на ID вашей Google Таблицы
+  const sheetID = '1G-3wbvdkQGlO64QLMe5gMd4M2epPzF4ohohhYBrb-oM'; // Замените на ID вашей Google Таблицы
   const apiKey = 'AIzaSyAoZ92_SYkSos0qZY7XXL6njvUVGkwCgFk'; // Замените на ваш API ключ
-  const range = 'Sheet1!A1:G11'; // Диапазон ячеек, который вы хотите получить
+  const range = 'Sheet1!A1:G11'; // Здесь замените на диапазон ячеек на нужном вам листе
 
   const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${range}?key=${apiKey}`;
 
@@ -12,36 +12,20 @@ function fetchDataFromGoogleSheet() {
       const values = data.values;
       const table = document.getElementById('data-table');
 
-      for (const entry of values) {
-        const row = document.createElement('tr');
-        for (const value of entry) {
-          const cell = document.createElement('td');
-          cell.textContent = value;
-          row.appendChild(cell);
+      for (const row of values) {
+        const tr = document.createElement('tr');
+        for (const cellValue of row) {
+          const td = document.createElement('td');
+          td.textContent = cellValue;
+          tr.appendChild(td);
         }
-        table.appendChild(row);
+        table.appendChild(tr);
       }
     })
     .catch((error) => console.log('Error fetching data:', error));
 }
 
-// Анимация появления таблицы
-function animateTable() {
-  const table = document.getElementById('data-table');
-  table.style.opacity = 0;
-
-  let opacity = 0;
-  const animationInterval = setInterval(() => {
-    opacity += 0.05;
-    table.style.opacity = opacity;
-    if (opacity >= 1) {
-      clearInterval(animationInterval);
-    }
-  }, 100);
-}
-
-// Получение данных и запуск анимации при загрузке страницы
+// Получение данных при загрузке страницы
 document.addEventListener('DOMContentLoaded', () => {
   fetchDataFromGoogleSheet();
-  animateTable();
 });
