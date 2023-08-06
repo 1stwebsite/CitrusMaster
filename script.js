c5179e3fa564c496ea2eea3684545e7fee188cd2
1stweb.site <64778271+1stwebsite@users.noreply.github.com>
2023-08-06 09:55:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,34 +2,46 @@
 function fetchDataFromGoogleSheet() {
   const sheetID = '1G-3wbvdkQGlO64QLMe5gMd4M2epPzF4ohohhYBrb-oM'; // Замените на ID вашей Google Таблицы
   const apiKey = 'AIzaSyAoZ92_SYkSos0qZY7XXL6njvUVGkwCgFk'; // Замените на ваш API ключ
-  const range = 'Sheet1!A1:G14'; // Диапазон ячеек, который вы хотите получить
+  const range = 'Sheet1!A1:G11'; // Диапазон ячеек, который вы хотите получить
 
   const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${range}?key=${apiKey}`;
 
   fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
+    .then((response) => response.json())
     .then((data) => {
-      if (data.values && data.values.length > 0) {
-        const values = data.values;
-        const table = document.getElementById('data-table');
+      const values = data.values;
+      const table = document.getElementById('data-table');
 
-        for (const entry of values) {
-          const row = document.createElement('tr');
-          for (const value of entry) {
-            const cell = document.createElement('td');
-            cell.textContent = value;
-            row.appendChild(cell);
-          }
-          table.appendChild(row);
+      for (const entry of values) {
+        const row = document.createElement('tr');
+        for (const value of entry) {
+          const cell = document.createElement('td');
+          cell.textContent = value;
+          row.appendChild(cell);
         }
-      } else {
-        console.log('No data available.');
+        table.appendChild(row);
       }
     })
     .catch((error) => console.log('Error fetching data:', error));
 }
+
+// Анимация появления таблицы
+function animateTable() {
+  const table = document.getElementById('data-table');
+  table.style.opacity = 0;
+
+  let opacity = 0;
+  const animationInterval = setInterval(() => {
+    opacity += 0.05;
+    table.style.opacity = opacity;
+    if (opacity >= 1) {
+      clearInterval(animationInterval);
+    }
+  }, 100);
+}
+
+// Получение данных и запуск анимации при загрузке страницы
+document.addEventListener('DOMContentLoaded', () => {
+  fetchDataFromGoogleSheet();
+  animateTable();
+});
