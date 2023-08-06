d3e745840626b17d1847847a3dab7a95a3d8f303
1stweb.site <64778271+1stwebsite@users.noreply.github.com>
2023-08-06 17:05:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Функция для получения данных из Google Таблицы
 function fetchDataFromGoogleSheet() {
   const sheetID = '1G-3wbvdkQGlO64QLMe5gMd4M2epPzF4ohohhYBrb-oM'; // Замените на ID вашей Google Таблицы
-  const apiKey = 'AIzaSyAoZ92_SYkSos0qZY7XXL6njvUVGkwCgFk'; // Замените на ваш API ключ
-  const range = 'ЦМ выполнение!A1:G11'; // Здесь замените на диапазон ячеек на нужном вам листе
+  const apiKey = 'YOUR_API_KEY'; // Замените на ваш API ключ
+  const range = 'Sheet1!A1:G11'; // Диапазон ячеек, который вы хотите получить
 
   const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${range}?key=${apiKey}`;
 
@@ -12,44 +12,32 @@ function fetchDataFromGoogleSheet() {
       const values = data.values;
       const table = document.getElementById('data-table');
 
-      for (const row of values) {
-        const tr = document.createElement('tr');
-        for (const cellValue of row) {
-          const td = document.createElement('td');
-          td.textContent = cellValue;
-          tr.appendChild(td);
+      for (const entry of values) {
+        const row = document.createElement('tr');
+        for (const value of entry) {
+          const cell = document.createElement('td');
+          cell.textContent = value;
+          row.appendChild(cell);
         }
-        table.appendChild(tr);
+        table.appendChild(row);
       }
     })
     .catch((error) => console.log('Error fetching data:', error));
 }
 
-// Получение данных при загрузке страницы
-document.addEventListener('DOMContentLoaded', () => {
-  fetchDataFromGoogleSheet();
-});
-// Функция для сортировки данных по столбцу
-function sortByColumn(columnIndex) {
+// Анимация появления таблицы
+function animateTable() {
   const table = document.getElementById('data-table');
-  const rows = Array.from(table.querySelectorAll('tr'));
-  const headerRow = rows.shift(); // Удаляем заголовочную строку из массива
+  table.style.opacity = 0;
 
-  rows.sort((a, b) => {
-    const aValue = a.cells[columnIndex].textContent;
-    const bValue = b.cells[columnIndex].textContent;
-    return aValue.localeCompare(bValue, undefined, { numeric: true });
-  });
-
-  table.innerHTML = ''; // Очищаем таблицу
-
-  table.appendChild(headerRow); // Добавляем заголовочную строку обратно
-  rows.forEach((row) => table.appendChild(row)); // Добавляем отсортированные строки
-}
-
-// Функция для получения данных из Google Таблицы
-function fetchDataFromGoogleSheet() {
-  // ... Ваш предыдущий код для получения данных
+  let opacity = 0;
+  const animationInterval = setInterval(() => {
+    opacity += 0.05;
+    table.style.opacity = opacity;
+    if (opacity >= 1) {
+      clearInterval(animationInterval);
+    }
+  }, 100);
 }
 
 // Получение данных и запуск анимации при загрузке страницы
